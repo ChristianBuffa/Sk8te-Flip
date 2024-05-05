0dc5826a33626e0cf2ddf0cc49473e119fa05f09
Christian Buffa <christian.b2402@gmail.com>
2024-05-05 20:14:52 +0200

    fixed camera follow script

--- Scenes/camera_follow.gd
+++ Scenes/camera_follow.gd
@@ -1,12 +1,19 @@
-extends Camera3D
+extends Node3D
 
-@export var follow_target: Node3D
+var direction = Vector3.FORWARD
+@export_range (1,10,0.1) var smooth_speed: float = 2.5
 
-@onready var camera_3d = $"."
-
-
-func _ready():
-	pass
 
 func _process(delta):
-	pass
+	var current_velocity = get_parent().get_linear_velocity()
+	current_velocity.y = 0 
+	
+	if current_velocity.length_squared() > 1:
+		direction = lerp(direction, -current_velocity.normalized(), smooth_speed * delta)
+	
+	global_transform.basis = get_rotation_from_direction(direction)
+
+func get_rotation_from_direction(look_direction: Vector3) -> Basis:
+	look_direction = look_direction.normalized()
+	var x_axis = look_direction.cross(Vector3.UP)
+	return Basis(x_axis, Vector3.UP, -look_direction)
