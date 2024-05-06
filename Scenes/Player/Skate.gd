ff4081f689ca7b2a33ce0d3ddcaa31f1d9f79890
LucaGunnella <luca.gunnella@gmail.com>
2024-05-06 12:54:38 +0200

    emote and idle

--- Scenes/Player/Skate.gd
+++ Scenes/Player/Skate.gd
@@ -3,7 +3,16 @@ extends RigidBody3D
 const JUMP_FORCE = 5
 const IMPULSE_FORCE = 10
 const SWERVE_FORCE = 5
-var ROTATION_ANGLE = 1
+const ROTATION_ANGLE = 1
+@onready var animation_player = $Character_Ragdoll/AnimationPlayer
+
+func _ready():
+	animation_player.play("idle")
+	
+
+func _process(delta):
+	if(Input.is_action_just_pressed("Emote 1")):
+		animation_player.play("emote-yes")
 
 func _physics_process(delta):
 	var is_on_floor = check_is_on_floor()
@@ -21,6 +30,7 @@ func _physics_process(delta):
 			apply_torque_impulse(Vector3(0, -ROTATION_ANGLE, 0) * delta)
 			
 		if Input.is_action_just_pressed("Flip"):
+			$SkateBoard_cs/AnimationPlayer.play("flip_animation")
 			apply_central_impulse(Vector3(0, JUMP_FORCE, 0))
 
 func check_is_on_floor() -> bool :
