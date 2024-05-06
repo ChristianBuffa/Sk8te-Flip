ae83a0ba33ece633c65ee4916ade8db66ed3fa59
Gioele Giusti <gioele.pg@gmail.com>
2024-05-06 16:15:13 +0200

    fix resize scenes

--- Scenes/Score.gd
+++ Scenes/Score.gd
@@ -3,15 +3,16 @@ extends Control
 var score = 0
 @onready var scoreLabel = $Score
 
-
 func _ready():
-	pass
-	
-func _process(delta):
-	pass
-		
+	reset_score()
 
+func reset_score():
+	score = 0
+	update_score_ui()
 
-func _on_point_system_update_score_ui(score):
-	pass # Replace with function body.
-	scoreLabel.text = "Score %d" %score
+func _on_point_system_update_score_ui(new_score):
+	score = new_score
+	update_score_ui()
+
+func update_score_ui():
+	scoreLabel.text = "Score: %d" % score
