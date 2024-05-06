2b5de735c35173ac9c2da6bf6331959b6faed0be
LucaGunnella <luca.gunnella@gmail.com>
2024-05-06 12:11:40 +0200

    add skate with rigidbody

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -1,31 +1,22 @@
-extends CharacterBody3D
-
+extends VehicleBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+const TORQUE_ANGLE = 10
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("SwerveLeft", "SwerveRight", "Accelerate", "Decelerate")
+
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		steering = input_dir.x * TORQUE_ANGLE;
+		linear_velocity.z = direction.z * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
-	move_and_slide()
+		linear_velocity.z = move_toward(linear_velocity.z, 0, SPEED)
